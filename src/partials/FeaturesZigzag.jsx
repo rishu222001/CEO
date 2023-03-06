07b129ca0c6d2adcdc020bd884c3c2cefcf1de58
Rishika Jhunjhunwala <102367428+rishu222001@users.noreply.github.com>
2023-03-06 11:22:06 +0530

    first commit

---
+++ src/partials/FeaturesZigzag.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+import FeatImage01 from '../images/features-03-image-01.png';
+import FeatImage02 from '../images/features-03-image-02.jpeg';
+import FeatImage03 from '../images/features-03-image-03.jpeg';
+import FeatImage04 from '../images/features-03-image-03.jpeg';
+
+function FeaturesZigzag() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+    
+             <h1 className="h2 mb-4">EVENT STRUCTURE</h1>
+           
+          </div>
+
+          {/* Items */}
+          <div className="grid gap-20">
+
+            {/* 1st item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+                <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage01} width="540" height="305" alt="Features 01"/>
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <h3 className="h3 mb-3"> Questionnaire Round</h3>
+                  <p className="text-xl text-white-400 mb-4">This round comprises of filling basic Questionnaire through google form.</p>
+                  
+                </div>
+              </div>
+            </div>
+
+            {/* 2nd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
+                <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage02} width="540" height="305" alt="Features 02" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                 
+                  <h3 className="h3 mb-3">Group Discussion</h3>
+                  <p className="text-xl text-white-400 mb-4">Participants will be provided with topic and top peformers from each group will be selected for next round</p>
+                 
+                </div>
+              </div>
+            </div>
+
+            {/* 3rd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+                <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage03} width="540" height="205" alt="Features 03" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+          
+                  <h3 className="h3 mb-3">Marketistaan</h3>
+                  <p className="text-xl text-white-400 mb-4">Marketistaan is market analysis case study where different teams representing our various coorporate professionals and participants provide solution to case study</p>
+                 
+                </div>
+              </div>
+            </div>
+                        {/* 4th item */}
+                        <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
+                <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage04} width="540" height="405" alt="Features 02" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                 
+                  <h3 className="h3 mb-3">The Boardroom Dillema</h3>
+                  <p className="text-xl text-white-400 mb-4">Each participant will be assigned a role in boardroom according to which participants have to discuss, raise ponts and give innovative ideas</p>
+                  
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default FeaturesZigzag;
---
+++ src/partials/Header.jsx
@@ -0,0 +1,96 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import FeatImage05 from '../images/E-Cell_white.png'
+
+function Header() {
+
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+  const trigger = useRef(null);
+  const mobileNav = useRef(null);
+
+  // close the mobile menu on click outside
+  useEffect(() => {
+    const clickHandler = ({ target }) => {
+      if (!mobileNav.current || !trigger.current) return;
+      if (!mobileNavOpen || mobileNav.current.contains(target) || trigger.current.contains(target)) return;
+      setMobileNavOpen(false);
+    };
+    document.addEventListener('click', clickHandler);
+    return () => document.removeEventListener('click', clickHandler);
+  });
+
+  // close the mobile menu if the esc key is pressed
+  useEffect(() => {
+    const keyHandler = ({ keyCode }) => {
+      if (!mobileNavOpen || keyCode !== 27) return;
+      setMobileNavOpen(false);
+    };
+    document.addEventListener('keydown', keyHandler);
+    return () => document.removeEventListener('keydown', keyHandler);
+  });
+
+  return (
+    <header className="absolute w-full z-30">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="flex items-center justify-between h-20">
+
+          {/* Site branding */}
+          <div className="shrink-0 mr-4">
+            {/* Logo */}
+            <Link to="/" className="block" aria-label="Cruip">
+            <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
+                <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage05} width="200" height="150" alt="Features 02" />
+              </div>
+            </Link>
+          </div>
+
+          {/* Desktop navigation */}
+          <nav className="hidden md:flex md:grow">
+
+            {/* Desktop sign in links */}
+            <ul className="flex grow justify-end flex-wrap items-center">
+              <li>
+                <Link to="/signin" className="font-medium text-grey-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">Log in</Link>
+              </li>
+              <li>
+                <Link to="/signup" className="btn-sm text-white bg-red-600 hover:bg-red-700 ml-3">Contact Us</Link>
+              </li>
+            </ul>
+
+          </nav>
+
+          {/* Mobile menu */}
+          <div className="md:hidden">
+
+            {/* Hamburger button */}
+            <button ref={trigger} className={`hamburger ${mobileNavOpen && 'active'}`} aria-controls="mobile-nav" aria-expanded={mobileNavOpen} onClick={() => setMobileNavOpen(!mobileNavOpen)}>
+              <span className="sr-only">Menu</span>
+              <svg className="w-6 h-6 fill-current text-gray-300 hover:text-gray-200 transition duration-150 ease-in-out" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <rect y="4" width="24" height="2" rx="1" />
+                <rect y="11" width="24" height="2" rx="1" />
+                <rect y="18" width="24" height="2" rx="1" />
+              </svg>
+            </button>
+
+            {/*Mobile navigation */}
+            <nav id="mobile-nav" ref={mobileNav} className="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out" style={mobileNavOpen ? { maxHeight: mobileNav.current.scrollHeight, opacity: 1 } : { maxHeight: 0, opacity: .8 } }>
+              <ul className="bg-gray-800 px-4 py-2">
+                <li>
+                  <Link to="/signin" className="flex font-medium w-full text-grey-600 hover:text-white-200 py-2 justify-center">Log in</Link>
+                </li>
+                <li>
+                  <Link to="/signup" className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-red-600 hover:bg-red-700 transition duration-150 ease-in-out">Contact Us</Link>
+                </li>
+              </ul>
+            </nav>
+
+          </div>
+
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
